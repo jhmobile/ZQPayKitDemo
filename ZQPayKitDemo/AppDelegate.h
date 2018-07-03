3789c5bd84678411bf010bd03de44dd0bd0e9392
mshqiu <mshqiu@rxhui.com>
2018-07-03 19:45:55 +0800

    Initial Commit

---
+++ ZQPayKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQPayKitDemo
+//
+//  Created by mshqiu on 2018/7/3.
+//  Copyright © 2018年 jinhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQPayKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZQPayKitDemo
+//
+//  Created by mshqiu on 2018/7/3.
+//  Copyright © 2018年 jinhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
